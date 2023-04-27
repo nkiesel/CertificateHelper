dda99903a46b0b71b8698e0c2b656eece3ab62e9
Norbert Kiesel <norbert.kiesel@creditkarma.com>
2023-04-26 17:42:38 -0700

    upgrade 3rd party libs and Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import kotlin.io.path.createDirectories
 import kotlin.io.path.writeText
 
 plugins {
-    kotlin("jvm") version "1.8.20"
+    kotlin("jvm") version "1.8.21"
     kotlin("plugin.serialization") version "1.8.21"
     application
 }
